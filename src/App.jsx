7b0f18fd30d6ce46748cea6da1de0503a4b961bf
prs.faridi <parisa.faridi80@gmail.com>
2024-11-10 14:26:18 +0330

    Add notFOund component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import Author from "./components/Author.jsx";
+import Blog from "./components/Blog.jsx";
 import HomePage from "./components/home/HomePage.jsx";
 import Layout from "./components/layout/Layout.jsx";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./components/NotFound.jsx";
 
 function App() {
   return (
     <>
       <Layout>
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blogs/:slug" element={<Blog />} />
+          <Route path="/authors/:slug" element={<Author />} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
       </Layout>
     </>
   );
