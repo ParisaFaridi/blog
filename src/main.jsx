e193b01046f1173e7eab416d6089af72e314eb21
prs.faridi <parisa.faridi80@gmail.com>
2024-11-10 14:28:21 +0330

    Add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App.jsx";
 import theme from "./mui/theme.js";
 import "./styles/index.css";
 import "./styles/fonts.css";
+import { BrowserRouter } from "react-router-dom";
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHQL_URI,
@@ -14,8 +15,10 @@ const client = new ApolloClient({
 
 createRoot(document.getElementById("root")).render(
   <ApolloProvider client={client}>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </ApolloProvider>
 );
